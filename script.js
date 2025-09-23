360d6107f51b9ec8d64cf4cc593ec556ff94d4bd
LnxW <110757601+LnxW@users.noreply.github.com>
2025-09-23 22:39:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
 // HERO SLIDES
 let currentSlide = 0;
 const slides = document.querySelectorAll(".hero img");
+const heroCaption = document.getElementById("hero-caption");
+const heroTexts = ["Second or never.", "I see red.", "Rising star."];
 
 function showSlide(n) {
   slides.forEach((slide, i) => {
     slide.classList.remove("active");
     if (i === n) slide.classList.add("active");
   });
+  heroCaption.textContent = heroTexts[n];
 }
 
 setInterval(() => {
   currentSlide = (currentSlide + 1) % slides.length;
   showSlide(currentSlide);
-}, 4000);
+}, 5000);
 
 showSlide(currentSlide);
 
 // MENU
-const menuTrigger = document.getElementById("menu-trigger");
-const menuLightbox = document.getElementById("menu-lightbox");
+const menuToggle = document.getElementById("menu-toggle");
+const navOverlay = document.getElementById("nav-overlay");
 
-menuTrigger.addEventListener("click", () => {
-  menuLightbox.classList.toggle("active");
+menuToggle.addEventListener("click", () => {
+  navOverlay.classList.toggle("active");
+});
+
+// SHOP LINK (buat visible bila klik menu)
+const shopLink = document.getElementById("shop-link");
+const shopSection = document.getElementById("shop");
+
+shopLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  shopSection.classList.remove("hidden"); // buka shop
+  navOverlay.classList.remove("active");  // tutup menu
+  document.querySelector("#shop").scrollIntoView({behavior:"smooth"});
 });
 
 // SHOP PREVIEW
 const shopPreview = document.getElementById("shop-preview");
-function openShopPreview() {
-  shopPreview.classList.add("active");
-}
-function closeShopPreview() {
-  shopPreview.classList.remove("active");
-}
+function openShopPreview(){shopPreview.classList.add("active");}
+function closeShopPreview(){shopPreview.classList.remove("active");}
