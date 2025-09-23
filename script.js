552d4cb76e7283ff69acb0d51dafd4e5ff57fc98
LnxW <110757601+LnxW@users.noreply.github.com>
2025-09-23 22:49:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// HERO SLIDES
-let currentSlide = 0;
-const slides = document.querySelectorAll(".hero img");
-const heroCaption = document.getElementById("hero-caption");
-const heroTexts = ["Second or never.", "I see red.", "Rising star."];
-
-function showSlide(n) {
-  slides.forEach((slide, i) => {
-    slide.classList.remove("active");
-    if (i === n) slide.classList.add("active");
-  });
-  heroCaption.textContent = heroTexts[n];
-}
-
-setInterval(() => {
-  currentSlide = (currentSlide + 1) % slides.length;
-  showSlide(currentSlide);
-}, 5000);
-
-showSlide(currentSlide);
-
 // MENU
 const menuToggle = document.getElementById("menu-toggle");
 const navOverlay = document.getElementById("nav-overlay");
@@ -27,17 +6,13 @@ menuToggle.addEventListener("click", () => {
   navOverlay.classList.toggle("active");
 });
 
-
-// SHOP LINK (buat visible bila klik menu)
-const shopLink = document.getElementById("shop-link");
-const shopSection = document.getElementById("shop");
-
-shopLink.addEventListener("click", (e) => {
-  e.preventDefault();
-  shopSection.classList.remove("hidden"); // buka shop
-  navOverlay.classList.remove("active");  // tutup menu
-  document.querySelector("#shop").scrollIntoView({behavior:"smooth"});
+// Close menu when link clicked
+document.querySelectorAll("#nav-overlay a").forEach(link => {
+  link.addEventListener("click", () => {
+    navOverlay.classList.remove("active");
+  });
 });
+
 
 // SHOP PREVIEW
 const shopPreview = document.getElementById("shop-preview");
