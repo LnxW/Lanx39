566890fec8f5ecd4c0234d2b53a3c6a00b581b8e
LnxW <110757601+LnxW@users.noreply.github.com>
2025-09-25 00:42:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -18,29 +18,26 @@ const heroBg = document.getElementById("hero-bg");
 const heroCaption = document.getElementById("hero-caption");
 
 const heroSlides = [
-  {img:"image/hero1.jpg", text:"Second or Never."},
-  {img:"image/hero2a.jpg", text:'I see <span class="red">red</span>.'},
+  {img:"image/hero1.jpg", text:"Second or never."},
+  {img:"image/hero2.jpg", text:"I see red.", color:"#ff0000"},
   {img:"image/hero3.jpg", text:"Rising star."}
 ];
 
 let heroIndex = 0;
 
 function showHero(){
-  heroBg.style.opacity = 0; // fade out
-  setTimeout(() => {
-    heroBg.style.backgroundImage = `url('${heroSlides[heroIndex].img}')`;
-    heroCaption.innerHTML = heroSlides[heroIndex].text;
-    heroBg.style.opacity = 1; // fade in
-    heroCaption.style.opacity = 0;
-    setTimeout(() => heroCaption.style.opacity = 1, 300);
-  }, 500);
-
-  heroIndex = (heroIndex + 1) % heroSlides.length;
+  heroBg.style.opacity = 0;
+  setTimeout(()=>{
+    heroBg.style.backgroundImage = url('${heroSlides[heroIndex].img}');
+    heroCaption.textContent = heroSlides[heroIndex].text;
+    heroCaption.style.color = heroSlides[heroIndex].color || "#fff";
+    heroBg.style.opacity = 1;
+  },500);
+  heroIndex = (heroIndex+1)%heroSlides.length;
 }
 
 showHero();
-setInterval(showHero, 4000);
-
+setInterval(showHero,4000);
 // SHOP PREVIEW
 const shopPreview = document.getElementById("shop-preview");
 const shopSlides = document.getElementById("shop-slides");
